2bd6c549b772c1c0264f0fe8e30acbfebea97f8d
elye <elye.project@gmail.com>
2022-01-26 18:43:18 +1100

    Add 2 core

--- app/src/main/java/com/example/mandelbrot/drawing/MandelbrotCustomCanvas.kt
+++ app/src/main/java/com/example/mandelbrot/drawing/MandelbrotCustomCanvas.kt
@@ -6,13 +6,13 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import java.util.concurrent.Flow
 
 class MandelbrotCanvasCustom {
 
     companion object {
         private const val MAX_ITER = 300
         private const val ZOOM = 300
+        private val CORE = 2
     }
 
     data class MandelbrotPoint(
@@ -22,49 +22,56 @@ class MandelbrotCanvasCustom {
 
     fun parallel(canvas: MandelbrotCanvas) = runBlocking {
         val consumer = Channel<MandelbrotPoint>()
-        accumulateProducer(consumer, canvas)
+
+        repeat(CORE) {
+            accumulateProducer(
+                consumer, canvas,
+                canvas.height.toInt() * it / CORE,
+                canvas.height.toInt() * (it + 1) / CORE
+            )
+        }
         accumulateConsumer(consumer).join()
     }
 
     fun CoroutineScope.accumulateProducer(
         consumer: Channel<MandelbrotPoint>,
-        canvas: MandelbrotCanvas) =
-        produce<Double>(Dispatchers.Default) {
-            for (y in 0 until canvas.height.toInt()) {
-                for (x in 0 until canvas.width.toInt()) {
+        canvas: MandelbrotCanvas,
+        start: Int,
+        end: Int
+    ) = produce<Double>(Dispatchers.Default) {
+        for (y in start until end) {
+            for (x in 0 until canvas.width.toInt()) {
 
-                    var zx = 0.0
-                    var zy = 0.0
-                    val cX = (x - canvas.width / 2) / ZOOM
-                    val cY = (y - canvas.height / 2) / ZOOM
-                    var iter = MAX_ITER
-                    while (zx * zx + zy * zy < 4.0 && iter > 0) {
-                        val tmp = zx * zx - zy * zy + cX
-                        zy = 2.0 * zx * zy + cY
-                        zx = tmp
-                        iter--
-                    }
-
-                    canvas.drawPoint(
-                        x.toFloat(), y.toFloat(),
-                        (x.toFloat() / canvas.width * 0xFF).toInt(),
-                        (y.toFloat() / canvas.height * 0xFF).toInt(),
-                        ((iter or (iter shl 7)).toFloat()
-                                / ((MAX_ITER or MAX_ITER shl 7)).toFloat() * 0xFF).toInt()
-                    )
+                var zx = 0.0
+                var zy = 0.0
+                val cX = (x - canvas.width / 2) / ZOOM
+                val cY = (y - canvas.height / 2) / ZOOM
+                var iter = MAX_ITER
+                while (zx * zx + zy * zy < 4.0 && iter > 0) {
+                    val tmp = zx * zx - zy * zy + cX
+                    zy = 2.0 * zx * zy + cY
+                    zx = tmp
+                    iter--
                 }
-            }
 
-            consumer.send(MandelbrotPoint(0f, 0f, 0, 0, 0))
+                canvas.drawPoint(
+                    x.toFloat(), y.toFloat(),
+                    (x.toFloat() / canvas.width * 0xFF).toInt(),
+                    (y.toFloat() / canvas.height * 0xFF).toInt(),
+                    ((iter or (iter shl 7)).toFloat()
+                            / ((MAX_ITER or MAX_ITER shl 7)).toFloat() * 0xFF).toInt()
+                )
+            }
         }
+
+        consumer.send(MandelbrotPoint(0f, 0f, 0, 0, 0))
+    }
 
     fun CoroutineScope.accumulateConsumer(consumer: Channel<MandelbrotPoint>) =
         launch(Dispatchers.Default) {
-            val x = consumer.receive()
+            repeat(CORE) { consumer.receive() }
             consumer.close()
         }
-
-
 
     fun draw(canvas: MandelbrotCanvas) {
         parallel(canvas)
